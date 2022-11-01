deac560227c569fb31df341e104d4443aaf4e950
Chukwuebuka Nwaturuba <chukwuebukaclement@yahoo.com>
2022-11-01 17:39:18 +0100

    updated my assignment

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,6 +1,16 @@
 function fizzBuzz(input){
-
-  
+if (input % 3) {
+    return "fizz";
+} 
+else if (input % 5) {
+    return "buzz";
+}
+else if (input % 3 && input % 5) {
+    return "fizzbuzz";
+}
+else {
+    return Number;
+}
 }
 
 
--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,4 +1,26 @@
 function calculateGrade(marks) {
- 
+ if(marks >= 1 && marks <= 49) {
+    return "F";
  }
+ else if(marks >= 50 && marks <= 59) {
+    return "E";
+ } 
+ else if(marks >= 60 && marks <= 69) {
+    return "D";
+ } 
+ else if(marks >= 70 && marks <= 79) {
+    return "C";
+ }
+ else if(marks >= 80 && marks <= 89) {
+    return "B";
+ }
+ else if(marks >= 90 && marks <= 100) {
+    return "a";
+ } else {
+    return "Input number between 1-100";
+ }
+ }
+ let result = calculateGrade(70);
+
+ console.log(result);
   module.exports =calculateGrade
