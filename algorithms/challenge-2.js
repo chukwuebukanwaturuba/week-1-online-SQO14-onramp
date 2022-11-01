cf41bc9347a7c33d8e6c94131f67acc37e5fdcf1
Chukwuebuka Nwaturuba <chukwuebukaclement@yahoo.com>
2022-11-01 21:05:52 +0100

    corrected challenge-2

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,11 +1,11 @@
 function fizzBuzz(input){
-if (input % 3) {
+if (input % 3 == 0) {
     return "fizz";
 } 
-else if (input % 5) {
+else if (input % 5 == 0) {
     return "buzz";
 }
-else if (input % 3 && input % 5) {
+else if (input % 3 == 0 && input % 5 == 0 ) {
     return "fizzbuzz";
 }
 else {
